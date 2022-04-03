f818566cbf21beb816588460a73cacd79067bd59
shivam <shivamguptaluck@gmail.com>
2022-04-03 14:56:00 +0530

    my first commit

---
+++ src/main/java/com/example/springtest/dao/Test.java
@@ -0,0 +1,18 @@
+package com.example.springtest.dao;
+
+import java.sql.Time;
+import java.time.LocalTime;
+
+public class Test {
+
+
+    public void run()
+    {
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(LocalTime.now());
+    }
+}
---
+++ src/test/java/com/example/springtest/DemoApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("great boy");
+	}
+
+
+	@Test
+	void tesging() {
+		System.out.println("great boy");
+	}
+}
